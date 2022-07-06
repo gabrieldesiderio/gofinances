543cf5b7c47f0b573fb8b0540830366d4fc3e999
Gabriel Desidério <gabrieldesd7@gmail.com>
2022-07-06 20:15:10 -0300

    Create SignIn screen

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import LogoSvg from "../../assets/logo.svg";
+import AppleSvg from "../../assets/apple.svg";
+import GoogleSvg from "../../assets/google.svg";
+
+import {
+  Container,
+  Header,
+  TitleWrapper,
+  Title,
+  SignInTitle,
+  Footer,
+  FooterWrapper
+} from "./styles";
+import { RFValue } from "react-native-responsive-fontsize";
+import { SignInSocialButton } from "../../components/SignInSocialButton";
+
+export function SignIn() {
+  return (
+    <Container>
+      <Header>
+        <TitleWrapper>
+          <LogoSvg width={RFValue(120)} height={RFValue(68)} />
+          <Title>
+            Controle suas {"\n"}
+            finanças de forma {"\n"}
+            muito simples
+          </Title>
+        </TitleWrapper>
+        <SignInTitle>
+          Faça seu login {"\n"}
+          com uma das contas abaixo
+        </SignInTitle>
+      </Header>
+      <Footer>
+        <FooterWrapper>
+          <SignInSocialButton 
+            svg={GoogleSvg} 
+            title="Entrar com Google" 
+          />
+          <SignInSocialButton 
+            svg={AppleSvg} 
+            title="Entrar com Apple" 
+          />
+        </FooterWrapper>
+      </Footer>
+    </Container>
+  );
+}
