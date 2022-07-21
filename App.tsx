26407751f9fe28f6923565a4f80e6c16d5228ed3
Gabriel Desidério <gabrieldesd7@gmail.com>
2022-07-21 12:08:20 -0300

    add stack navigation

--- App.tsx
+++ App.tsx
@@ -1,26 +1,23 @@
-import 'intl';
-import 'intl/locale-data/jsonp/pt-BR';
+import "intl";
+import "intl/locale-data/jsonp/pt-BR";
 
-import React from 'react';
-import { StatusBar } from 'react-native'
-import { ThemeProvider } from 'styled-components' 
-import * as SplashScreen from 'expo-splash-screen'
+import React from "react";
+import { StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components";
+import * as SplashScreen from "expo-splash-screen";
 
 import {
   useFonts,
   Poppins_400Regular,
   Poppins_500Medium,
-  Poppins_700Bold
-} from '@expo-google-fonts/poppins'
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
 
-import theme from './src/global/styles/theme';
+import theme from "./src/global/styles/theme";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { AppRoutes } from './src/routes/app.routes';
+import { Routes } from "./src/routes";
 
-import { SignIn } from './src/screens/SignIn';
-
-import { AuthProvider } from './src/hooks/auth'
+import { AuthProvider } from "./src/hooks/auth";
 
 export default function App() {
   SplashScreen.preventAutoHideAsync();
@@ -28,10 +25,10 @@ export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_500Medium,
-    Poppins_700Bold
+    Poppins_700Bold,
   });
 
-  if(!fontsLoaded) {
+  if (!fontsLoaded) {
     return null;
   }
 
@@ -39,13 +36,10 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <NavigationContainer>
-        <StatusBar barStyle="light-content" />
-        {/* <AppRoutes /> */}
-        <AuthProvider>
-          <SignIn />
-        </AuthProvider>
-      </NavigationContainer>
+      <StatusBar barStyle="light-content" />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
-  )
+  );
 }
